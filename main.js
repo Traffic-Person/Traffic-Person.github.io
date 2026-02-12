2b7b66e9f79815dc54c093ce5aee13500820cb35
Traffic-Person <eryk.barczak@gmail.com>
2026-02-12 13:58:58 +0100

    hm

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-const button = document.getElementById("counter");
 
-// get saved count or start at 0
-let count = localStorage.getItem("count");
-count = count ? parseInt(count) : 0;
-
-// update button text
-button.value = "Count: " + count;
-
-button.onclick = () => {
-    count++;
-    localStorage.setItem("count", count);
-    button.value = "Count: " + count;
-};
